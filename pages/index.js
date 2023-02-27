91a956beb265566556b9b4def4ffd74d109435c3
Ondrej Sika <ondrej@ondrejsika.com>
2023-02-27 13:50:00 +0000

    Deleted main.go, package.json, yarn.lock, xxxx, test_example.py, .gitignore, pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-let Home = () => {
-  return <h1>Hello from Next.js</h1>;
-}
-
-export default Home;
-
--- test_example.py
+++
@@ -1,7 +0,0 @@
-
-# test_example.py
-def test_ok():
-    assert 1
-
-def test_err():
-    assert 0
